f482a6930fa62a24cf6c96af5de17a9674648dd5
GABRIEL TALLES LOPES <gabriel.tl@puccampinas.edu.br>
2019-10-22 18:41:31 -0300

    including delete user partial

---
+++ WindowsFormsApp1/ExcluirUser.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class ExclusaoUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSenha = new System.Windows.Forms.TextBox();
+            this.tbCpf = new System.Windows.Forms.MaskedTextBox();
+            this.gbLogin = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gbLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.tbSenha.Location = new System.Drawing.Point(16, 131);
+            this.tbSenha.Name = "textBox1";
+            this.tbSenha.Size = new System.Drawing.Size(100, 20);
+            this.tbSenha.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.tbCpf.Location = new System.Drawing.Point(16, 72);
+            this.tbCpf.Name = "maskedTextBox1";
+            this.tbCpf.Size = new System.Drawing.Size(100, 20);
+            this.tbCpf.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.gbLogin.Controls.Add(this.button1);
+            this.gbLogin.Controls.Add(this.label2);
+            this.gbLogin.Controls.Add(this.label1);
+            this.gbLogin.Controls.Add(this.tbSenha);
+            this.gbLogin.Controls.Add(this.tbCpf);
+            this.gbLogin.Location = new System.Drawing.Point(23, 30);
+            this.gbLogin.Name = "groupBox1";
+            this.gbLogin.Size = new System.Drawing.Size(196, 257);
+            this.gbLogin.TabIndex = 2;
+            this.gbLogin.TabStop = false;
+            this.gbLogin.Text = "Faça o login novamente para continuar a exclusão";
+            this.gbLogin.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbLogin);
+            this.Name = "Form1";
+            this.Text = "Exclusão de usuario";
+            this.gbLogin.ResumeLayout(false);
+            this.gbLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSenha;
+        private System.Windows.Forms.MaskedTextBox tbCpf;
+        private System.Windows.Forms.GroupBox gbLogin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ WindowsFormsApp1/ExcluirUser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class ExclusaoUser : Form
+    {
+        public ExclusaoUser()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoginUsecase loginUsecase = new LoginUsecase();
+
+
+            try
+            {
+                User user = loginUsecase.Login(tbCpf.Text, tbSenha.Text);
+                if (user==null) throw new Exception();
+
+                gbLogin.Hide();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao autenticar");
+            }
+        }
+    }
+}
