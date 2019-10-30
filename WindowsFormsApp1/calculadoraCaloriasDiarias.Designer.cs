a4307a3d8c53494c6f2aadf87552830cf9584504
GABRIEL TALLES LOPES <gabriel.tl@puccampinas.edu.br>
2019-10-30 18:24:18 -0300

    initial form to calculate day consume

--- WindowsFormsApp1/calculadoraCaloriasDiarias.Designer.cs
+++ WindowsFormsApp1/calculadoraCaloriasDiarias.Designer.cs
@@ -31,12 +31,38 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
+            
+            
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+           
+            this.dataGridView1.Location = new System.Drawing.Point(58, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(708, 283);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // calculadoraCaloriasDiarias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "calculadoraCaloriasDiarias";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "calculadoraCaloriasDiarias";
+            this.Load += new System.EventHandler(this.calculadoraCaloriasDiarias_Load);
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- WindowsFormsApp1/calculadoraCaloriasDiarias.cs
+++ WindowsFormsApp1/calculadoraCaloriasDiarias.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
@@ -7,6 +8,11 @@ namespace WindowsFormsApp1
         public calculadoraCaloriasDiarias()
         {
             InitializeComponent();
+        }
+
+        private void calculadoraCaloriasDiarias_Load(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
